4c3456e248bc558d31768525a8c9065effc2d28b
collerty <nhlsviatoslav@gmail.com>
2025-03-12 17:10:33 +0100

    fixed nestj build dir

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+import { json } from 'body-parser';
+import { getBodyParserOptions } from '@nestjs/platform-express/adapters/utils/get-body-parser-options.util';
+import { urlencoded } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  const app = await NestFactory.create(AppModule, {
+  });
+
+  app.use(express.json());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    credentials: true,
+    origin: configService.get<string>('NEXT_URL_PRODUCTION'),
+  });
+  const PORT = configService.get<number>('PORT') || 4000;
+  await app.listen(PORT);
 }
+
 bootstrap();
