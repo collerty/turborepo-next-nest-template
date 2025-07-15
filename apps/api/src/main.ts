98059f3bb4f6efa782906e122742a3ff7944e209
collerty <collerty@collertys-MacBook-Air.local>
2025-07-15 10:46:01 +0200

    feat: prisma query adapter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
-import { json } from 'body-parser';
-import { getBodyParserOptions } from '@nestjs/platform-express/adapters/utils/get-body-parser-options.util';
-import { urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,9 +1,40 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const databaseUrl = process.env.DATABASE_URL;
+
+    // Type checking for databaseUrl
+    if (!databaseUrl) {
+      throw new Error(
+        'DATABASE_URL environment variable is not set. Please check your .env file.',
+      );
+    }
+
+    const adapter = new PrismaPg({
+      connectionString: databaseUrl,
+      // You can add other pg.Pool options here if needed, e.g.:
+      // max: 20, // set max number of clients in the pool
+      // idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    });
+
+    super({
+      adapter: adapter,
+      log: ['query', 'info', 'warn', 'error'], // Optional: for logging queries
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
