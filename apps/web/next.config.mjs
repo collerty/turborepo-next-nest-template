f288a0444fcb0a574eb2c2f8453e59950d81ae64
collerty <nhlsviatoslav@gmail.com>
2025-09-09 13:54:41 +0200

    fix: externalDir: true

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const config = {
   transpilePackages: ['@workspace/ui'],
+  experimental: { externalDir: true },
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "lh3.googleusercontent.com" }, // Google profile pics
-      { protocol: "https", hostname: "avatars.githubusercontent.com" }, // GitHub
-      { protocol: "https", hostname: "pbs.twimg.com" }, // Twitter/X
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' }, // Google profile pics
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' }, // GitHub
+      { protocol: 'https', hostname: 'pbs.twimg.com' }, // Twitter/X
     ],
   },
 };
