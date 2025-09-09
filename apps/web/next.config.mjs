88eeeb58f9633cc009e1950da3ea001e50538b3c
collerty <nhlsviatoslav@gmail.com>
2025-09-09 12:57:43 +0200

    fix: nextjs config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,3 @@
-import { createMDX } from 'fumadocs-mdx/next';
-
-const withMDX = createMDX();
-
 /** @type {import('next').NextConfig} */
 const config = {
   transpilePackages: ['@workspace/ui'],
@@ -13,5 +9,4 @@ const config = {
     ],
   },
 };
-
-export default withMDX(config);
+export default config;
