1a47b4cc4ffc5788a9bc2bd0f8508414b704b1a9
collerty <nhlsviatoslav@gmail.com>
2025-09-09 13:21:23 +0200

    fix: add transpile package to /docs config

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   basePath: '/docs',
+  transpilePackages: ['@workspace/ui'],
   typescript: {
     ignoreBuildErrors: true,
   },
